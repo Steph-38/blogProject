174feff8aac7f883f43e1df7dc567b81d3412603
Steph <stephchevalier38@gmail.com>
2018-12-17 12:47:42 +0100

    Modification du CSS

--- view/template.php
+++ view/template.php
@@ -9,16 +9,22 @@ session_start();
     	<link href="public/style.css" rel="stylesheet" />
     </head>
     <body>
-		<h1>WELCOME TO THE BLOG !</h1>
+		
+		<header>
+    		<h1>WELCOME TO THE BLOG !</h1>
+    		<p>
+    			<a href="index.php">Accueil</a>
+    			<a href="index.php?action=viewMember">Inscription</a>
+    			<a href="index.php?action=viewLogin">Connexion</a>
+    			<a href="index.php?action=logout">Déconnexion</a>
+    		</p>
+		</header>
+
 		<?php
 		if (isset($_SESSION['id']) && isset($_SESSION['pseudo'])) {
 		    echo $_SESSION['pseudo'] . ' est avec nous !';
 		}
 		?>
-		<p><a href="index.php">Retour à la liste des billets</a></p>
-    	<p><a href="index.php?action=viewMember">Inscription</a></p>
-    	<p><a href="index.php?action=viewLogin">Connexion</a></p>
-    	<p><a href="index.php?action=logout">Déconnexion</a></p>
     	<?= $content ?>
     </body>
 </html>
