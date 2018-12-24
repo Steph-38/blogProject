1d24e115919f595ac9ea970a1220ff40994396dc
Steph <stephchevalier38@gmail.com>
2018-12-24 13:32:46 +0100

    Test de mise en forme de la barre de navigation avec flexbox

--- view/template.php
+++ view/template.php
@@ -13,7 +13,11 @@ session_start();
 		<header>
     		<h1>WELCOME TO THE BLOG !</h1>
     		<div class=navigation>
-    			<a href="index.php">Accueil</a><a href="index.php?action=viewMember">Inscription</a><a href="index.php?action=viewLogin">Connexion</a><a href="index.php?action=logout">Déconnexion</a><a href="https://fr.lipsum.com/" title="Lien vers le site lipsum.com" target="_blank">Lorem</a>
+    			<a href="index.php">Accueil</a>
+    			<a href="index.php?action=viewMember">Inscription</a>
+    			<a href="index.php?action=viewLogin">Connexion</a>
+    			<a href="index.php?action=logout">Déconnexion</a>
+    			<a href="https://fr.lipsum.com/" title="Lien vers le site lipsum.com" target="_blank">Lorem</a>
 			</div>
     		<?php
     		if (isset($_SESSION['id']) && isset($_SESSION['pseudo'])) {
